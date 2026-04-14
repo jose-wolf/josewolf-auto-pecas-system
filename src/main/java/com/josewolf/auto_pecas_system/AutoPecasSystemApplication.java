8755a07356c6e072cfe0a7a23188913f3ac2ed01
José Wolf <wolf.jose89@gmail.com>
2026-04-13 23:19:03 -0300

    Primeiro commit

---
+++ src/main/java/com/josewolf/auto_pecas_system/AutoPecasSystemApplication.java
@@ -0,0 +1,13 @@
+package com.josewolf.auto_pecas_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPecasSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPecasSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josewolf/auto_pecas_system/AutoPecasSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josewolf.auto_pecas_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPecasSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
